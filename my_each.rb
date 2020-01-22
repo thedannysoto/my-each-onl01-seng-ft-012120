f3b910cb9553e07397faad1092bbba2554f085e5
Danny Soto <87dsoto@gmail.com>
2020-01-22 20:33:49 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(word)
+  counter = 0;
+  while counter < word.length
+    yield(word[counter])
+    counter += 1
+  end
+  return word
 end
